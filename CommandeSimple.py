61d55f54f8d0b134fbf4fa5b6d40b8de2722d3bf
GravenDiscordPy <50522998+GravenDiscordPy@users.noreply.github.com>
2019-05-11 12:21:47 -0300

    Create CommandeSimple.py

---
+++ CommandeSimple.py
@@ -0,0 +1,29 @@
+import discord 
+from discord.ext import commands
+bot = commands.Bot(commands.when_mentioned_or('.'))
+
+@bot.event
+async def on_ready():
+	print("Bot Connecté")
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await ctx.message.channel.send("pong !")
+    
+client.run("<Token>")
+
+Ou
+
+import discord
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print("Bot connecté !")
+
+@client.event
+async def on_message(message):
+	if message.content == '!infos':
+		await message.channel.send('Infos : \nNom : ' + client.user.name + "\nID : " + str(client.user.id))
+    
+client.run("<Token>")
