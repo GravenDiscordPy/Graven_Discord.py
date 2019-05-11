2fe33988060dc6c1f5ddefa91c39873d6af8e092
GravenDiscordPy <50522998+GravenDiscordPy@users.noreply.github.com>
2019-05-11 19:41:03 -0300

    Create StatusBot.py

---
+++ Commandes/StatusBot.py
@@ -0,0 +1,14 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print("Bot connecté !")
+
+@client.event
+async def on_message(message):
+	if message.content == '!deco':
+		await client.change_presence(status=discord.Status("offline"))
+  if message.content == '!co':
+    await client.change_presence(status=discord.Status("online"))
