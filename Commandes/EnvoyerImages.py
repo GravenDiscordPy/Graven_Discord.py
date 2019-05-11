267146db5e21c5af41ef3725d0afb2b0da3e876b
GravenDiscordPy <50522998+GravenDiscordPy@users.noreply.github.com>
2019-05-11 16:02:43 -0300

    Create EnvoyerImages.py

---
+++ Commandes/EnvoyerImages.py
@@ -0,0 +1,15 @@
+import discord
+from random import choice
+
+listeimages = ["test.jpg","image.png"] #Les images doivent être dans le fichier, sinon "images/test.jpg" par exemple.
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("Bot connecté !")
+async def on_message(message):
+	if message.author == client.user:
+		return
+	if message.content == '!image':
+	  await message.channel.send(file=discord.File(choice(listeimages)))
