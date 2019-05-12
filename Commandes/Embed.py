46ad587c031a4996f210c23ccf511997edfea194
GravenDiscordPy <50522998+GravenDiscordPy@users.noreply.github.com>
2019-05-11 21:52:33 -0300

    Update Embed.py

--- Commandes/Embed.py
+++ Commandes/Embed.py
@@ -29,4 +29,13 @@ async def ping(ctx):
     e = discord.Embed(title="Pong", description="!!!!!!")
     await ctx.message.channel.send(embed=e)# On dit qu'on envoie une embed et que l'emebed est 'e' (la variable)
     
+###########################################
+
+#Embed avancé
+@bot.command(pass_context=True)
+async def infos(ctx):
+    e = discord.Embed(title="Infos",colour=discord.Colour.from_rgb(174,244,254),description='Nom : ' + client.user.name + "\nID : " + str(client.user.id) + "\nPréfix : " + prefix[0]).set_image(url=client.user.avatar_url)
+    await ctx.message.channel.send(embed=e)
+    #Embed avec un titre, une description, une couleur et une image
+
 bot.run("<Token>")
